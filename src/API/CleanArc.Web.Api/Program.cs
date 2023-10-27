837d2ce15e9ce8d3e492ab485d824a77d5a8c447
Moataz Dakhlaoui <m.daklaui@yahoo.com>
2023-10-28 01:53:25 +0200

    Ajoutez des fichiers projet.

---
+++ src/API/CleanArc.Web.Api/Program.cs
@@ -0,0 +1,102 @@
+using System.Diagnostics;
+using CleanArc.Application.ServiceConfiguration;
+using CleanArc.Domain.Entities.User;
+using CleanArc.Infrastructure.CrossCutting.Logging;
+using CleanArc.Infrastructure.Identity.Identity.Dtos;
+using CleanArc.Infrastructure.Identity.Identity.SeedDatabaseService;
+using CleanArc.Infrastructure.Identity.Jwt;
+using CleanArc.Infrastructure.Identity.ServiceConfiguration;
+using CleanArc.Infrastructure.Persistence;
+using CleanArc.Infrastructure.Persistence.ServiceConfiguration;
+using CleanArc.SharedKernel.Extensions;
+using CleanArc.Web.Api.Controllers.V1.UserManagement;
+using CleanArc.WebFramework.Filters;
+using CleanArc.WebFramework.Middlewares;
+using CleanArc.WebFramework.ServiceConfiguration;
+using CleanArc.WebFramework.Swagger;
+using Microsoft.EntityFrameworkCore;
+using Serilog;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Host.UseSerilog(LoggingConfiguration.ConfigureLogger);
+
+var configuration = builder.Configuration;
+
+Activity.DefaultIdFormat = ActivityIdFormat.W3C;
+
+builder.Services.Configure<IdentitySettings>(configuration.GetSection(nameof(IdentitySettings)));
+
+var identitySettings = configuration.GetSection(nameof(IdentitySettings)).Get<IdentitySettings>();
+
+builder.Services.AddControllers(options =>
+{
+    options.Filters.Add(typeof(OkResultAttribute));
+    options.Filters.Add(typeof(NotFoundResultAttribute));
+    options.Filters.Add(typeof(ContentResultFilterAttribute));
+    options.Filters.Add(typeof(ModelStateValidationAttribute));
+    options.Filters.Add(typeof(BadRequestResultFilterAttribute));
+
+}).ConfigureApiBehaviorOptions(options =>
+{
+    options.SuppressModelStateInvalidFilter = true;
+    options.SuppressMapClientErrors = true;
+});
+
+builder.Services.AddSwagger();
+
+builder.Services.AddApplicationServices()
+    .RegisterIdentityServices(identitySettings)
+    .AddPersistenceServices(configuration)
+    .AddWebFrameworkServices();
+
+builder.Services.RegisterValidatorsAsServices();
+
+
+
+builder.Services.AddAutoMapper(typeof(User), typeof(JwtService), typeof(UserController));
+
+var app = builder.Build();
+
+
+#region Seeding and creating database
+
+await using (var scope = app.Services.CreateAsyncScope())
+{
+    var context = scope.ServiceProvider.GetService<ApplicationDbContext>();
+
+    if (context is null)
+        throw new Exception("Database Context Not Found");
+
+    await context.Database.MigrateAsync();
+
+
+    var seedService = scope.ServiceProvider.GetRequiredService<ISeedDataBase>();
+    await seedService.Seed();
+}
+
+#endregion
+
+#region Pipleline Configuration
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+
+app.UseCustomExceptionHandler();
+
+
+app.UseSwaggerAndUI();
+
+app.UseRouting();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllers();
+
+await app.RunAsync();
+#endregion
+
+
