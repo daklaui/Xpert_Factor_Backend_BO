1609b483df86ad0c4c4ed8b2726b35a6eadce3a3
AmineKhadhraoui <aminekhadhraoui2017@gmail.com>
2024-05-03 19:59:03 +0100

    committing to check another branch

--- src/Infrastructure/CleanArc.Infrastructure.Persistence/Repositories/RecouvrementRepository.cs
+++ src/Infrastructure/CleanArc.Infrastructure.Persistence/Repositories/RecouvrementRepository.cs
@@ -2,6 +2,7 @@ using CleanArc.Application.Common;
 using CleanArc.Application.Contracts.Persistence;
 using CleanArc.Domain.Entities;
 using CleanArc.Infrastructure.Persistence.Repositories.Common;
+using Microsoft.EntityFrameworkCore;
 
 namespace CleanArc.Infrastructure.Persistence.Repositories;
 
@@ -17,8 +18,24 @@ internal class RecouvrementRepository: BaseAsyncRepository<TR_LIST_VAL>, IRecouv
     {
         await base.AddAsync(Recouvrement);
     }
-
-
+    public async Task<bool> UpdateDocumentDetBordAsync(int id , TR_LIST_VAL updatedRecouvrement)
+    {
+        var existingRecouvrement = await GetRecouvrementById(id);
+        existingRecouvrement.ABR_LIST_VAL = updatedRecouvrement.ABR_LIST_VAL;
+        existingRecouvrement.TYP_LIST_VAL = updatedRecouvrement.TYP_LIST_VAL;
+        existingRecouvrement.ORD_LIST_VAL = updatedRecouvrement.ORD_LIST_VAL;
+        existingRecouvrement.LIB_LIST_VAL = updatedRecouvrement.LIB_LIST_VAL;
+        existingRecouvrement.COM_LIST_VAL = updatedRecouvrement.COM_LIST_VAL;
+        existingRecouvrement.NB_JOUR_LIST_VAL = updatedRecouvrement.NB_JOUR_LIST_VAL;
+        existingRecouvrement.TYPE_RECOUVREMENT = updatedRecouvrement.TYPE_RECOUVREMENT;
+        _dbContext.Entry(existingRecouvrement).State = EntityState.Modified;
+        await _dbContext.SaveChangesAsync();
+        return true;
+    }
+    public async Task<TR_LIST_VAL> GetRecouvrementById(int id)
+    {
+        return await base.TableNoTracking.FirstAsync(p=>p.ID_LIST_VAL == id);
+    }
     public async Task<PagedList<TR_LIST_VAL>> GetAllRecAsync(PaginationParams paginationParams)
     {
         var query = base.TableNoTracking.AsQueryable().Where(p => p.TYP_LIST_VAL == "COMM_RECOV"); 
