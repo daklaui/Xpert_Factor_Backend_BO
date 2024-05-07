d482ff20b65d60a8a85e886f38ed091348561471
Anis <you@example.com>
2024-05-07 09:55:55 +0100

    auth push

---
+++ src/Core/CleanArc.Domain/Entities/User/UserROLES.cs
@@ -0,0 +1,10 @@
+﻿namespace CleanArc.Domain.Entities.User;
+
+public class UserROLES
+{
+    public int UserId { get; set; }
+    public TS_USER User { get; set; }
+
+    public int RoleId { get; set; }
+    public Role Role { get; set; }
+}
